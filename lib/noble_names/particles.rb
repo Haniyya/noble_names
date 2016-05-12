68cc253a0ec0640d364c9f6787d6c32c1a7db9e8
Paul Martensen <stu102532@mail.uni-kiel.de>
2016-05-12 20:55:06 +0200

    Removed debugging puts.

--- lib/noble_names/particles.rb
+++ lib/noble_names/particles.rb
@@ -4,7 +4,6 @@ module NobleNames
   SUPPORTED_LANGUAGES = [:german,:english,:french,:spanish,:portuguese]
 
   DATA_PATH = File.expand_path '../../../data/', __FILE__
-  p File.expand_path('particles.yml', DATA_PATH)
 
   PARTICLES = YAML.load_file(File.expand_path(
                                'particles.yml', DATA_PATH
