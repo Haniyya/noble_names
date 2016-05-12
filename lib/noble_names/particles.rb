ee230692c3462b1f46d6470b9fac8960055661c6
Paul Martensen <stu102532@mail.uni-kiel.de>
2016-05-12 12:46:58 +0200

    Added Particle loader

---
+++ lib/noble_names/particles.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+
+module NobleNames
+  DATA_PATH = File.expand_path '../../../data/', __FILE__
+  p File.expand_path('particles.yml',DATA_PATH)
+
+  PARTICLES = YAML.load_file(File.expand_path(
+    'particles.yml',DATA_PATH
+  ))['particles']
+end
