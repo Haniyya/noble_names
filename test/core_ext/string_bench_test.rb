f2525c6374156773fdc73af1d92c7070168ec43e
Paul Martensen <stu102532@mail.uni-kiel.de>
2016-05-27 10:27:00 +0200

    Excluded benchmarks from normal tests.

--- test/core_ext/string_bench_test.rb
+++ test/core_ext/string_bench_test.rb
@@ -3,15 +3,17 @@ require_relative '../test_helper'
 require 'minitest/benchmark'
 
 class TestString < Minitest::Benchmark
-  def bench_particle_linear
-    assert_performance_linear 0.99 do |n|
-      ('james of windsor ' * n).to_title
+  if ENV['BENCH']
+    def bench_particle_linear
+      assert_performance_linear 0.99 do |n|
+        ('james of windsor ' * n).to_title
+      end
     end
-  end
 
-  def bench_prefix_linear
-    assert_performance_linear 0.99 do |n|
-      ('james mcdormer ' * n).to_title
+    def bench_prefix_linear
+      assert_performance_linear 0.99 do |n|
+        ('james mcdormer ' * n).to_title
+      end
     end
   end
 end
